76b83f80f96da9f9ab8f8d3347f606fae2eec203
Donaldoo <donald.kumaraku@gmail.com>
2022-09-09 05:06:23 -0700

    Task 6

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,13 +2,12 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
-    if argv != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     a = int(argv[1])
-    b = int(argv[3])
     operator = argv[2]
-    print("{}".format(argv))
+    b = int(argv[3])
     if operator == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif operator == "-":
