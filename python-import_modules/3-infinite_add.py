08b4dec1f48be32fd612d5e9cbf8f3884a6d1c98
Donaldoo <donald.kumaraku@gmail.com>
2022-09-09 02:27:21 -0700

    Task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    for i in range(argv):
-        sum += int(argv[i])
+    i = 1
+    args_sum = 0
+    while i != len(argv):
+        args_sum += int(argv[i])
+        i += 1
+    print("{}".format(args_sum))
