0a5850d3d147c3b17678b3cfa30f7a6f3c092f6f
Donaldoo <donald.kumaraku@gmail.com>
2022-09-12 05:15:42 -0700

    Task 10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    for i in my_list:
+        div = []
+        if i % 2 == 0:
+            div.append(True)
+        else:
+            div.append(False)
+    return div
