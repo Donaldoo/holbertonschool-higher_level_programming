e4e537ec1e45a2b9f527cabcc38ba794a82ae36e
Donaldoo <donald.kumaraku@gmail.com>
2022-09-13 05:36:30 -0700

    Task 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def print_sorted_dictionary(my_dict):
+    if my_dict is None:
+        return
+    for key, i in sorted(my_dict.items()):
+        print("{}: {}".format(key, i))
