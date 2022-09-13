0cc1d84fd93a0ceb341e6468970fa44006e30bef
Donaldoo <donald.kumaraku@gmail.com>
2022-09-13 05:55:41 -0700

    Task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if not a_dictionary or len(a_dictionary) == 0:
         return None
     return sorted(a_dictionary, key=lambda score: score[2])[-1]
