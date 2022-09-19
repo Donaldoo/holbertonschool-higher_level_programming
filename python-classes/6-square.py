acb898256bc65f0b1a405aeb7fe047a72b459ddf
Donaldoo <donald.kumaraku@gmail.com>
2022-09-19 12:51:29 -0700

    Fixed print function

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -38,13 +38,14 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print("")
-        x = self.position[0]
-        y = self.position[1]
-        for i in range(y):
-            print("")
-        for row in range(self.size):
-            for j in range(x):
-                print(" ", end="")
-            for column in range(self.size):
-                print("#", end="")
-            print("")
+        else:
+            x = self.position[0]
+            y = self.position[1]
+            for i in range(y):
+                print("")
+            for row in range(self.size):
+                for j in range(x):
+                    print(" ", end="")
+                for column in range(self.size):
+                    print("#", end="")
+                print("")
