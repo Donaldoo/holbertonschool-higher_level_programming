2487701a9aa5c59e471e2281f66589d2783d44f0
Donaldoo <donald.kumaraku@gmail.com>
2022-09-19 12:11:55 -0700

    Pep8

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -12,6 +12,10 @@ class Square:
     def size(self):
         return self.__size
 
+    @property
+    def position(self):
+        return self.__position
+
     @size.setter
     def size(self, value):
         if type(value) is not int:
@@ -20,15 +24,11 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    @property
-    def position(self):
-        return self.__position
-
     @position.setter
     def position(self, value):
         if type(value) is not tuple or len(value) != 2 or \
-            type(value[0]) is not int or type(value[1]) is not int or \
-            value[0] < 0 or value[1] < 0:
+                type(value[0]) is not int or type(value[1]) is not int or \
+                value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
