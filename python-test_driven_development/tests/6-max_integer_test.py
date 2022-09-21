7628b15ab5beeaa16bac29bb56829efa32e1356a
Donaldoo <donald.kumaraku@gmail.com>
2022-09-21 04:05:18 -0700

    update

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -7,15 +7,15 @@ class TestMaxInteger(unittest.TestCase):
     """unittet for max integer"""
     def test_max_at_end(self):
         numbers = [1, 2, 4, 3, 5]
-        self.assertEqual(max_integer(number), 5)
+        self.assertEqual(max_integer(numbers), 5)
 
     def test_max_at_beginning(self):
         numbers = [5, 2, 3, 4]
-        self.assertEqual(max_integer(number), 5)
+        self.assertEqual(max_integer(numbers), 5)
 
     def test_max_in_middle(self):
         numbers = [1, 2, 5, 4, 3]
-        self.assertEqual(max_integer(number), 5)
+        self.assertEqual(max_integer(numbers), 5)
 
     def test_negative_in_list(self):
         number = [1, 2, -3, 5]
