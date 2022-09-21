6ca120a9d33bc2f57588ea69d0b3ef6ff1a18cc7
Donaldoo <donald.kumaraku@gmail.com>
2022-09-21 02:38:19 -0700

    Pep8

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,16 +6,15 @@ def text_indentation(text):
     """prints text with 2 new lines afetr ".", "?", :"""
     if type(text) is not str:
         raise TypeError("text must be a string")
-        
+
     symbols = [".", "?", ":"]
     new_str = ""
     for i in range(len(text)):
-        if text[i] == " " and flag == True:
+        if text[i] == " " and flag == 1:
             continue
-        flag = False
+        flag = 0
         new_str += text[i]
         if text[i] in symbols:
             new_str += "\n" + "\n"
-            flag = True
+            flag = 1
     print(new_str, end="")
-
