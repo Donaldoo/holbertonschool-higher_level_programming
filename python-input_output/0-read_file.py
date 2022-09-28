e388069269d6a0e411969a24ddf3ae5bb19111b3
Donaldoo <donald.kumaraku@gmail.com>
2022-09-28 10:44:56 -0700

    Task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Task 0"""
 
 
 def read_file(filename=""):
+    """Opens and read a file"""
     with open(filename, encoding="utf-8") as myFile:
         readData = myFile.read()
         print(readData, end="")
