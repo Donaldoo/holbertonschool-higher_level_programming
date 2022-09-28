5f21ff000b7b844f915b91b9f5e0b38902d96dc5
Donaldoo <donald.kumaraku@gmail.com>
2022-09-28 13:50:26 -0700

    Task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Task 10"""
+
+
+class Student:
+    """New class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            new_dict = {}
+            for el in attrs:
+                if el in self.__dict__:
+                    new_dict[el] = self.__dict__[el]
+            return new_dict
+        return self.__dict__
