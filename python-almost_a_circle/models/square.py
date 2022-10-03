50cdda613d6802f9ab26c558e0e734ad9ec52d0e
Donaldoo <donald.kumaraku@gmail.com>
2022-10-03 11:51:02 -0700

    Task 11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,6 +8,15 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.height
+
+    @size.setter
+    def size(self, size):
+        self.__width = size
+        self.__height = size
+
     def __str__(self):
         """str method"""
         return "[Square] ({}) {}/{} - {}".format(
