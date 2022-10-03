87758874c672f058a4051e30f7c508a9b8953c32
Donaldoo <donald.kumaraku@gmail.com>
2022-10-03 11:45:38 -0700

    Task 10

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,8 +4,10 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """New class"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """str method"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.height)
