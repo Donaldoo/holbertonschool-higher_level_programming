3a7863463f023ad33b541ef9ee9096c7e42fe067
Donaldoo <donald.kumaraku@gmail.com>
2022-10-05 04:59:07 -0700

    update

---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import unittest
+from models.rectangle import Rectangle
+
+class TestRectangle(unittest.TestCase):
+    """Test class for rectangle class"""
+    def rectangle_width_height(self):
+        """create a rectangle with height and height"""
+        self.rect1 = Rectangle(1, 2)
+
+    def rectangle_width_height_x(self):
+        """create a rectangle with heigh, weight and x"""
+        self.rect1 = Rectangle(1, 2, 3)
+
+    def rectangle_width_height_x_y(self):
+        """create a rectangle with height, width, x, y"""
+        self.rect1 = Rectangle(1, 2, 3, 4)
+
+    def rectangle_width_str(self):
+        """test width as str"""
+        self.rect1 = Rectangle("one", 2)
+
+    def rectangle_height_str(self):
+        """test height as  str"""
+        self.rect1 = Rectangle(1, "two")
