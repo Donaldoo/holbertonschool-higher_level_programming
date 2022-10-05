644f9dd73fac48fc4cbb81826597e2e7604bd8a8
Donaldoo <donald.kumaraku@gmail.com>
2022-10-05 04:00:15 -0700

    update

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -4,12 +4,28 @@ from models.base import Base
 
 
 class TestBase(unittest.TestCase):
+    """Test class for Base class"""
     def test_if_id_none(self):
-    """test if id is none"""
+        """test if id is none"""
         base1 = Base()
         self.assertEqual(1, base1.id)
 
     def test_id(self):
         """test if id is not none"""
         base1 = Base(89)
-        self.asserEqual(89, base1.id)
+        self.assertEqual(89, base1.id)
+
+    def test_id_negative(self):
+        """test if id is negative"""
+        base1 = Base(-89)
+        self.assertEqual(-89. base1.id)
+
+    def test_id_string(self):
+        """test if id is string"""
+        base1 = Base("id")
+        self.assertEqual("id", base1.id)
+
+    def test_id_zero(self):
+        """test if id is 0"""
+        base1 = Base(0)
+        self.asserEqual(0, base1.id)
