4a54a1fccfd4b24347990fe5671a30df811c7e79
Donaldoo <donald.kumaraku@gmail.com>
2022-10-05 10:21:38 -0700

    update x and y cases

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,20 +7,19 @@ class TestRectangle(unittest.TestCase):
     def test_rectangle_width_height(self):
         """test rectangle with height and height"""
         rect1 = Rectangle(1, 2)
-        rect2 = Rectangle(2, 1)
-        self.assertEqual(rect1.id, rect2.id - 1)
+        self.assertEqual(1, rect1.id)
 
 
     def test_rectangle_width_height_x(self):
         """test rectangle with heigh, weight and x"""
         rect1 = Rectangle(1, 2, 3)
-        rect2 = Rectangle(4, 5, 6)
+        rect2 = Rectangle(3, 2, 1)
         self.assertEqual(rect1.id, rect2.id - 1)
 
     def test_rectangle_width_height_x_y(self):
         """test rectangle with height, width, x, y"""
         rect1 = Rectangle(1, 2, 3, 4)
-        rect2 = Rectangle(5, 3, 2, 1)
+        rect2 = Rectangle(2, 3, 4, 5)
         self.assertEqual(rect1.id, rect2.id - 1)
 
     def test_rectangle_all_args(self):
@@ -34,9 +33,19 @@ class TestRectangle(unittest.TestCase):
             rect1 = Rectangle("one", 2)
 
     def test_rectangle_height_str(self):
-        """test height as  str"""
+        """test height as str"""
         with self.assertRaises(TypeError):
             rect1 = Rectangle(1, "two")
+
+    def test_rect_x_str(self):
+        """test rect with x as str"""
+        with self.assertRaises(TypeError):
+            rect1 = Rectangle(1, 2, "three")
+
+    def test_rect_y_str(self):
+        """test rect with y as str"""
+        with self.assertRaises(TypeError):
+            rect1 = Rectangle(1, 2, 3, "four")
 
     def test_negative_width(self):
         """test rect with negative width"""
@@ -47,6 +56,16 @@ class TestRectangle(unittest.TestCase):
         """test rect with negative height"""
         with self.assertRaises(ValueError):
             rect1 = Rectangle(1, -2)
+
+    def test_neg_x(self):
+        """test rect with negative x"""
+        with self.assertRaises(ValueError):
+            rect1 = Rectangle(1, 2, -3)
+
+    def test_neg_y(self):
+        """test rect with negative y"""
+        with self.assertRaises(ValueError):
+            rect1 = Rectangle(1, 2, 3, -4)
 
     def test_width_zero(self):
         """test rect with width 0"""
