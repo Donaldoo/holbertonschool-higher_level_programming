424f81e96f8c4fc4fadced17d5d6f7def20bdf2a
Donaldoo <donald.kumaraku@gmail.com>
2022-10-05 03:40:12 -0700

    Added case

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -7,4 +7,9 @@ class TestBase(unittest.TestCase):
     def test_if_id_none(self):
     """test if id is none"""
         base1 = Base()
-        self.assertEqual(1, b.id)
+        self.assertEqual(1, base1.id)
+
+    def test_id(self):
+        """test if id is not none"""
+        base1 = Base(89)
+        self.asserEqual(89, base1.id)
