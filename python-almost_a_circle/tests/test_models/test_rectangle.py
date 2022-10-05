d8aa4eb7e61bc31c9d81b4c2981985c3f18930c6
Donaldoo <donald.kumaraku@gmail.com>
2022-10-05 09:56:06 -0700

    update

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -6,20 +6,29 @@ class TestRectangle(unittest.TestCase):
     """Test class for rectangle class"""
     def test_rectangle_width_height(self):
         """create a rectangle with height and height"""
-        self.rect1 = Rectangle(1, 2)
+        rect1 = Rectangle(1, 2)
+        rect2 = Rectangle(2, 1)
+        self.assertEqual(rect1.id, rect2.id - 1)
+
 
     def test_rectangle_width_height_x(self):
         """create a rectangle with heigh, weight and x"""
-        self.rect1 = Rectangle(1, 2, 3)
+        rect1 = Rectangle(1, 2, 3)
+        rect2 = Rectangle(4, 5, 6)
+        self.assertEqual(rect1.id, rect2.id - 1)
 
     def test_rectangle_width_height_x_y(self):
         """create a rectangle with height, width, x, y"""
-        self.rect1 = Rectangle(1, 2, 3, 4)
+        rect1 = Rectangle(1, 2, 3, 4)
+        rect2 = Rectangle(5, 3, 2, 1)
+        self.assertEqual(rect1.id, rect2.id - 1)
 
     def test_rectangle_width_str(self):
         """test width as str"""
-        self.rect1 = Rectangle("one", 2)
+        with self.assertRaises(TypeError):
+            rect1 = Rectangle("one", 2)
 
     def test_rectangle_height_str(self):
         """test height as  str"""
-        self.rect1 = Rectangle(1, "two")
+        with self.assertRaises(TypeError):
+            rect1 = Rectangle(1, "two")
