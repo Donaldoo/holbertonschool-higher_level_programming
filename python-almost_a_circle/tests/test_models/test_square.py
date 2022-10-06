d8fb606f8e2709f2779e4d0f5f765efd3cff4330
Donaldoo <donald.kumaraku@gmail.com>
2022-10-06 08:35:10 -0700

    update case

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -56,3 +56,33 @@ class TestSquare(unittest.TestCase):
     def test_zero_size(self):
         with self.assertRaises(ValueError):
             sq1 = Square(0)
+
+    def test_str_method(self):
+        sq1 = Square(3, 1, 2, 10)
+        self.assertEqual("[Square] (10) 1/2 - 3", sq1.__str__())
+
+    def test_update_method1(self):
+        sq1 = Square(3, 1, 2, 10)
+        sq1.update(89)
+        self.assertEqual(89, sq1.id)
+
+    def test_update_method2(self):
+        sq1 = Square(3, 1, 2, 10)
+        sq1.update(89, 1)
+        self.assertEqual(89, sq1.id)
+        self.assertEqual(1, sq1.size)
+
+    def test_update_method2(self):
+        sq1 = Square(3, 1, 2, 10)
+        sq1.update(89, 1, 2)
+        self.assertEqual(89, sq1.id)
+        self.assertEqual(1, sq1.size)
+        self.assertEqual(2, sq1.x)
+
+    def test_update_method2(self):
+        sq1 = Square(3, 1, 2, 10)
+        sq1.update(89, 1, 2, 3)
+        self.assertEqual(89, sq1.id)
+        self.assertEqual(1, sq1.size)
+        self.assertEqual(2, sq1.x)
+        self.assertEqual(3, sq1.y)
