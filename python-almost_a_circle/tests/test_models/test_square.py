a47f7af1b5efd31265959fe89a650321c612c51d
Donaldoo <donald.kumaraku@gmail.com>
2022-10-06 08:44:52 -0700

    update

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -86,3 +86,28 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(1, sq1.size)
         self.assertEqual(2, sq1.x)
         self.assertEqual(3, sq1.y)
+
+    def test_to_dect(self):
+        sq1 = Square(3, 1, 2, 10)
+        dict1 = {'size': 3, 'x': 1, 'y': 2, 'id': 10}
+        self.assertEqual(dict1, sq1.to_dictionary())
+
+    def test_create(self):
+        dict1 = {'size': 3, 'x': 1, 'y': 2, 'id': 10}
+        sq1 = Square.create(**dict1)
+
+    def test_save_to_file_None(self):
+        Square.save_to_file(None)
+        with open("Square.json", "r") as my_file:
+            self.assertEqual("[]", my_file.read())
+
+    def test_save_to_file_empty(self):
+        Square.save_to_file([])
+        with open("Square.json", "r") as my_file:
+            self.assertEqual("[]", my_file.read())
+
+    def test_load_from_file(self):
+        sq1 = Square(3, 1, 2, 10)
+        Square.save_to_file([sq1])
+        sqList = Square.load_from_file()
+        self.assertEqual(str(sq1), str(sqList[0]))
