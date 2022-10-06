22fbb60aadf01d4fad740b9221699153614d2f25
Donaldoo <donald.kumaraku@gmail.com>
2022-10-06 08:16:20 -0700

    update square cases

---
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import unittest
+from models.square import Square
+
+
+class TestSquare(unittest.TestCase):
+    """Test class for square class"""
+    def test_square_one(self):
+        """test 1 arg"""
+        sq1 = Square(1)
+        sq2 = Square(2)
+        self.assertEqual(sq1.id, sq2.id - 1)
+
+    def test_square_two(self):
+        """test 2 args"""
+        sq1 = Square(1, 2)
+        sq2 = Square(2, 3)
+        self.assertEqual(sq1.id, sq2.id - 1)
+
+    def test_square_three(self):
+        """test 3 args"""
+        sq1 = Square(1, 2, 3)
+        sq2 = Square(3, 4, 5)
+        self.assertEqual(sq1.id, sq2.id - 1)
+
+    def test_square_str_size(self):
+        with self.assertRaises(TypeError):
+            sq1 = Square("one")
