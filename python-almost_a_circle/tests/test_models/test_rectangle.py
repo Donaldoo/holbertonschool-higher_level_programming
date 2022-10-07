1e9be152fbdaa6933a919e38011b76b35d2324b2
Donaldoo <donald.kumaraku@gmail.com>
2022-10-07 01:03:03 -0700

    update

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -165,7 +165,7 @@ class TestRectangle(unittest.TestCase):
             string = my_file.read()
         self.assertEqual(str, type(string))
 
-    def test_save_to_file_none(self):
+    def test_save_to_file_None(self):
         Rectangle.save_to_file(None)
         with open("Rectangle.json", "r") as my_file:
             self.assertEqual("[]", my_file.read())
