620e669fed6d164a082b9e12e217471ba6a04009
Donaldoo <donald.kumaraku@gmail.com>
2022-11-09 05:55:12 -0800

    Task 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name\
                 FROM cities, states\
-                WHERE cities.states_id = states.id\
+                WHERE states.id = cities.state_id\
                 ORDER BY cities.id")
     rows = cur.fetchall()
     for row in rows:
