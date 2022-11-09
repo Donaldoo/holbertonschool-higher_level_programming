e21580d89d12ae190216bbf3a8c3b3761b866a06
Donaldoo <donald.kumaraku@gmail.com>
2022-11-09 06:05:01 -0800

    Task 5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""lists all cities from database"""
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities, states\
+                WHERE states.id = cities.state_id AND states.name = %s\
+                ORDER BY cities.id", (sys.argv[4], ))
+    rows = cur.fetchall()
+    for row in rows:
+        print(", ", end="")
+        print(row[0], end="")
+    print()
+    cur.close()
+    db.close()
