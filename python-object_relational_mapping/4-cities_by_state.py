3368ddbef070b67e6182095ef4949a6803695c25
Donaldoo <donald.kumaraku@gmail.com>
2022-11-09 05:43:51 -0800

    Task 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""lists all cities from database"""
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities JOIN states\
+                ON cities.states_id = states.id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
