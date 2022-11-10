299a9a0729bb4df6b37c01e182803d06f9fc66c6
Donaldoo <donald.kumaraku@gmail.com>
2022-11-10 05:31:55 -0800

    Task 11

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """adds new state to the database"""
 
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
