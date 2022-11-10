d5057dbebf632cfbb1581fb5ca8f60de34f9c819
Donaldoo <donald.kumaraku@gmail.com>
2022-11-10 05:09:56 -0800

    Task 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""lists all State objecs from database hbtn_0e_6_usa"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+    engine = create_engine(f"mysql+mysqldb://{argv[1]}:{argv[2]}\
+                           @localhost/{argv[3]}")
+    with Session(engine) as session:
+        statements = session.query(State).filter(State.name == sys.argv[4]).first()
+        if statements:
+            print(f"{statements.id}")
+        else:
+            print("Not found")
