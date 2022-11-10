8ae5f83fe790dd882b501cbe89a42b8cb350f755
Donaldoo <donald.kumaraku@gmail.com>
2022-11-10 05:51:20 -0800

    task 14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""lists all State objecs from database hbtn_0e_6_usa"""
+"""lists all State objects from database hbtn_0e_6_usa"""
 
 from sys import argv
 from model_state import Base, State
@@ -11,7 +11,6 @@ if __name__ == "__main__":
     engine = create_engine(f"mysql+mysqldb://{argv[1]}:{argv[2]}\
                            @localhost/{argv[3]}")
     with Session(engine) as session:
-        statements = session.query(State, City).\
-                   filter(State.id == City.state_id).order_by(City.id)
+        statements = session.query(State, City).filter(State.id == City.state_id).order_by(City.id)
         for state, city in statements:
             print(f"{state.name}: ({city.id}) {city.name}")
