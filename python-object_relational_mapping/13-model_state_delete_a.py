a9e3763e1afabe711a104f1514cc579fb6721223
Donaldoo <donald.kumaraku@gmail.com>
2022-11-10 05:37:52 -0800

    task 13

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""updates a state from the database"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+    engine = create_engine(f"mysql+mysqldb://{argv[1]}:{argv[2]}\
+                           @localhost/{argv[3]}")
+    with Session(engine) as session:
+        session.query(State).filter(State.name.like('%a%')).\
+                delete(synchronize_session='fetch')
+        session.commit()
