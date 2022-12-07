ab6117446307b55ddcba47483c8cfdd5906d6894
Donaldoo <donald.kumaraku@gmail.com>
2022-12-07 04:42:31 -0800

    task 11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 
-const list = process.argv;
-list.splice(0, 2);
+const list = process.argv.slice(2);
 const length = list.length;
-let ndLargest = 0;
 
 if (length === 0 || length === 1) {
-  console.log('0');
+  console.log(0);
 } else {
-  list.sort();
-  ndLargest = list[length - 2];
-  console.log(ndLargest);
+  list.sort((a, b) => a - b);
+  console.log(list[list.length - 2]);
 }
