9bd8e83700412778422ae8c1e497673c97d280c3
Donaldoo <donald.kumaraku@gmail.com>
2022-12-15 11:44:02 -0800

    task 8

--- javascript-web_jquery/8-script.js
+++ javascript-web_jquery/8-script.js
@@ -1,5 +1,5 @@
 $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
 	for (movie of data['results']) {
-		$('#list_movies').append('<li>' + movie['title'] + '<li>');
+		$('#list_movies').append('<li>' + movie['title'] + '</li>');
 	}
 });
